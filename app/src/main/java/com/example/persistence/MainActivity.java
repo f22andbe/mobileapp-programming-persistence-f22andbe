cacc9dfa567c92ece90337d89f3f7f062ecb7c85
f22andbe <f22andbe@student.his.se>
2023-05-19 20:00:14 +0200

    Create menu to clear view and database instead of using buttons. Also add some comments

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,14 +51,17 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(flowerAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-    /* click handler for read button */
+    
+    /* Fetch contents of database and display in a view */
     public void onClickRead(View v) {
+        //Log.d("onClickRead_top:", flowerList.toString());
         flowerList.clear();
         flowerQuery.selectFlowers();
         flowerAdapter.notifyDataSetChanged();
-        Log.d("onClickRead:", flowerList.toString());
+        //Log.d("onClickRead:", flowerList.toString());
     }
 
+    /* write the contents of the EditText fields to the database */
     public void onClickWrite(View v) {
         /* get data */
         String name = editFlowerName.getText().toString();
@@ -64,29 +69,67 @@ public class MainActivity extends AppCompatActivity {
         String genus = editFlowerGenus.getText().toString();
         long ret;
 
-        Log.d("onClickWrite:", "name = " + name + " species = " + species + " genus = " + genus);
+        //Log.d("onClickWrite:", "name = " + name + " species = " + species + " genus = " + genus);
 
         /* insert data in database */
         ret = flowerInsert.insertFlowerData(name, species, genus);
 
         /*clear edit fields */
-        editFlowerName.setText("");
-        if(ret == -1)
+        if(ret == -1) {
             editFlowerName.setText("Problem writing to database");
+        }else {
+            editFlowerName.setText("");
+        }
         editFlowerSpecies.setText("");
         editFlowerGenus.setText("");
         flowerAdapter.notifyDataSetChanged();
     }
 
-    public void onClickEmpty(View v) {
-        flowerQuery.deleteAllFlowerRows();
-        flowerList.clear();
-        flowerAdapter.notifyDataSetChanged();
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    public void onClickEmptyView(View v) {
-        flowerList.clear();
-        flowerAdapter.notifyDataSetChanged();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        /* Delete all rows in the database */
+        if (id == R.id.action_empty_database) {
+            Log.d("onOptionItemSelected","Empty database");
+            /* empty the database */
+            showDialog();
+            return true;
+        }
+
+        /* clear the view showing the contents of the database */
+        if (id == R.id.action_empty_view) {
+            Log.d("onOptionItemSelected","clear view");
+            /* clear the view */
+            flowerList.clear();
+            flowerAdapter.notifyDataSetChanged();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialog() {
+        ConfirmDialog dialogFragment = new ConfirmDialog();
+
+        /* Would have been a cleaner design to just get information from
+         * showDialog about which button was pressed and take action accordingly
+         * but I have yet to figure out how to do that so, we register these objects
+         * with the dialog and do the work in the click listener.
+         */
+        dialogFragment.setFlowerList(flowerList);
+        dialogFragment.setFlowerQuery(flowerQuery);
+        dialogFragment.setAdapter(flowerAdapter);
+        dialogFragment.show(getSupportFragmentManager(), "ConfirmDialog");
     }
 }
